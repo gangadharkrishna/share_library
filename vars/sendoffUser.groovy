e4fd46d2dbe0d2d41e7e3b0b8b04a2e47ed26db3
gangadharkrishna <87365263+gangadharkrishna@users.noreply.github.com>
2021-07-21 18:19:32 +0530

    Update sendoffUser.groovy

--- vars/sendoffUser.groovy
+++ vars/sendoffUser.groovy
@@ -2,14 +2,14 @@ def call(String designation = 'null') {
 	echo "This is shared library sendoffUser: ${DESG}"
 	pipeline {
 		environment {
-			DESG = "${designation}"
+			DESG_NAME = "${DESG}"
 		}
 		agent any 
 		stages {
 			stage('SENDOF_USER') {
 				steps {
 					//echo "Bye $USER_NAME, Nice meeting you !"
-					echo "Designation is  $DESG"
+					echo "Designation is  $DESG_NAME"
 				}	
 			}
 		}
