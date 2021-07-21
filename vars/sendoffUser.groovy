8d06f2fd737db6d071ee7ab43e848aaed2ab64e4
gangadharkrishna <87365263+gangadharkrishna@users.noreply.github.com>
2021-07-21 17:34:28 +0530

    Update sendoffUser.groovy

--- vars/sendoffUser.groovy
+++ vars/sendoffUser.groovy
@@ -9,7 +9,7 @@ def call(String designation = 'null') {
 		stages {
 			stage('SENDOF_USER') {
 				steps {
-					echo "Bye $USER_NAME, Nice meeting you !"
+					//echo "Bye $USER_NAME, Nice meeting you !"
 					echo "Designation: $DESG"
 				}	
 			}
